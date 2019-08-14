41bef86b223f52a8223bb5af8ff71058742c602d
ValdemarRuby <wings6928@gmail.com>
2019-08-14 11:04:39 +0300

    Add card view and method parse orders input

--- app.rb
+++ app.rb
@@ -19,5 +19,29 @@ get '/order' do
 end
 
 post '/card' do
+	orders_input = params[:orders]
+	@orders = parse_orders_input orders_input
 	erb :card
+end
+
+
+def parse_orders_input orders_input
+	s1 = orders_input.split(/,/)
+
+	arr = []
+
+	s1.each do |x|
+		s2 = x.split(/\=/)
+
+		s3 = s2[0].split(/_/)
+
+		id = s3[1]
+		cnt = s2[1]
+
+		arr2 = [id, cnt]
+
+		arr.push arr2
+	end
+
+	return arr2
 end
